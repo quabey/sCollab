45d6003b88054b0f98ab8f5870ffe505b32f98fa
quabey <quadrobtv@gmail.com>
2022-11-26 23:20:22 +0100

    better css, update stores

--- src/Components/openList.svelte
+++ src/Components/openList.svelte
@@ -13,16 +13,18 @@
 
 <div class="">
     {#each list[openListID].lItems as item }
-    <div class="flex flex-row justify-between p-2 border-b-2 mx-2 h">
+    <div class="flex flex-row justify-between p-2 border-b-2 mx-2 items-center">
         <div class="">
-            <img src="{item.iIcon}" alt="Icon">
+            <img src="{item.iIcon}" alt="Icon" width="40px">
         </div>
-        <div class="">
+        <div class="text-2xl bg-green-400 place-content-center align-middle ">
             {item.iName}
-            {item.iAmount}
+            
         </div>
-        <div class="">
-            <label class="container">
+        <div class="flex place-content-center">
+            <div class="mr-5 bg-green-400 text-2xl rounded-full px-2 align-middle">{item.iAmount}</div>
+            
+            <label class="container scale-125">
                 <input type="checkbox" bind:checked={$lists[openListID].lItems[item.iID].iDone}> 
                 <span class="checkmark"></span>
             </label>
--- src/stores.js
+++ src/stores.js
@@ -4,19 +4,19 @@ export let selectedList =writable(2);
 
 export let lists = writable([
     {lID: 0, lName: "Personal", lIcon: "https://cdn-icons-png.flaticon.com/512/151/151917.png", lItems: [
-        {iID: 0, iName: "Milk", iDone: false, iAmount: 2, iIcon: ""},
-        {iID: 1, iName: "Bread", iDone: false, },
-        {iID: 2, iName: "Eggs", iDone: false, amount: 12}
+        {iID: 0, iName: "Milk", iDone: false, iAmount: 2, iIcon: "https://cdn-icons-png.flaticon.com/512/2662/2662503.png"},
+        {iID: 1, iName: "Bread", iDone: false, iAmount: 1, iIcon: "https://cdn-icons-png.flaticon.com/512/2662/2662503.png"},
+        {iID: 2, iName: "Eggs", iDone: false, iAmount: 12, iIcon: "https://cdn-icons-png.flaticon.com/512/2662/2662503.png"}
     ]},
     {lID: 1, lName: "Work", lIcon: "https://cdn-icons-png.flaticon.com/512/151/151917.png", lItems: [
-        {iID: 0, iName: "Cake", iDone: false, iAmount: 2, iIcon: ""},
-        {iID: 1, iName: "Water", iDone: false, iAmount: 1, iIcon: ""},
-        {iID: 2, iName: "Muffins", iDone: false, iAmount: 12, iIcon: ""}
+        {iID: 0, iName: "Cake", iDone: false, iAmount: 2, iIcon: "https://cdn-icons-png.flaticon.com/512/2662/2662503.png"},
+        {iID: 1, iName: "Water", iDone: false, iAmount: 1, iIcon: "https://cdn-icons-png.flaticon.com/512/2662/2662503.png"},
+        {iID: 2, iName: "Muffins", iDone: false, iAmount: 12, iIcon: "https://cdn-icons-png.flaticon.com/512/2662/2662503.png"}
     ]},
     {lID: 2, lName: "WG", lIcon: "https://cdn-icons-png.flaticon.com/512/151/151917.png", lItems: [
-        {iID: 0, iName: "Beer", iDone: true, iAmount: 2, iIcon: ""},
-        {iID: 1, iName: "Chrips", iDone: false, iAmount: 1, iIcon: ""},
-        {iID: 2, iName: "Vodka", iDone: false, iAmount: 12, iIcon: ""}
+        {iID: 0, iName: "Beer", iDone: true, iAmount: 2, iIcon: "https://cdn-icons-png.flaticon.com/512/2662/2662503.png"},
+        {iID: 1, iName: "Chrips", iDone: false, iAmount: 1, iIcon: "https://cdn-icons-png.flaticon.com/512/2662/2662503.png"},
+        {iID: 2, iName: "Vodka", iDone: false, iAmount: 12, iIcon: "https://cdn-icons-png.flaticon.com/512/2662/2662503.png"}
     ]}
 
 ]);
